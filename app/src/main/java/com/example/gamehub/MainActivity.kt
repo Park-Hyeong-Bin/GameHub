2165f150f46a41708b497b766c8b1b65e2809372
agbrgh <agbrgh@gmail.com>
2023-04-09 14:24:51 +0900

    로그인 기본구현

--- app/src/main/java/com/example/gamehub/MainActivity.kt
+++ app/src/main/java/com/example/gamehub/MainActivity.kt
@@ -1,11 +1,21 @@
 package com.example.gamehub
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.gamehub.databinding.ActivityMainBinding
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        if(Firebase.auth.currentUser == null){
+            startActivity(Intent(this, LoginActivity::class.java))
+            finish()
+        }
     }
 }
