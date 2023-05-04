b656d6604d182f0037baa035621169c4f3a1c1ec
agbrgh <agbrgh@gmail.com>
2023-05-05 01:35:04 +0900

    이름검색 프로토버전

---
+++ app/src/main/java/com/example/gamehub/FindAdapter.kt
@@ -0,0 +1,61 @@
+package com.example.gamehub
+
+import android.graphics.BitmapFactory
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.example.gamehub.databinding.HomeItemBinding
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
+import com.google.firebase.storage.StorageReference
+import com.google.firebase.storage.ktx.storage
+
+
+class FindViewHolder(val binding: HomeItemBinding ) : RecyclerView.ViewHolder(binding.root)
+
+
+
+class FindAdapter(private val itemList: ArrayList<String>) :
+    RecyclerView.Adapter<FindViewHolder>() {
+
+    private val storage = Firebase.storage
+    private val db = Firebase.firestore
+    // onCreateViewHolder: ViewHolder 객체 생성
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): FindViewHolder {
+        val binding = HomeItemBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return FindViewHolder(binding)
+    }
+
+    // onBindViewHolder: ViewHolder에 데이터 바인딩
+    override fun onBindViewHolder(holder: FindViewHolder, position: Int) {
+        val item = itemList[position]
+        val gameDB = db.collection("game").document(item).get().addOnSuccessListener {
+            val imagepath = it["imagepath"].toString()
+            val imageRef = storage.getReferenceFromUrl("gs://ghub-da878.appspot.com/${imagepath}")
+            println(imagepath)
+            val description = it["description"].toString()
+
+            displayImageRef(imageRef, holder.binding.imageHomeGame)
+            holder.binding.textHomeGame.text = description
+            println(description)
+        }
+    }
+
+    // getItemCount: 데이터 아이템 개수 반환
+    override fun getItemCount(): Int {
+        return itemList.size
+    }
+
+    private fun displayImageRef(imageRef: StorageReference?, view: ImageView){
+        imageRef?.getBytes(Long.MAX_VALUE)?.addOnSuccessListener {
+            val bmp = BitmapFactory.decodeByteArray(it, 0, it.size)
+            view.setImageBitmap(bmp)
+        }?.addOnFailureListener(){
+            println("Image load failed")
+        }
+
+    }
+
+}
--- app/src/main/java/com/example/gamehub/FindFragment.kt
+++ app/src/main/java/com/example/gamehub/FindFragment.kt
@@ -1,24 +1,76 @@
 package com.example.gamehub
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.SearchView
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.gamehub.databinding.FragmentFindBinding
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
 
 class FindFragment : Fragment() {
 
     private lateinit var binding: FragmentFindBinding
+    private val db = Firebase.firestore
+    private var itemList = ArrayList<String>()
+    private lateinit var adapter: FindAdapter
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
         binding = FragmentFindBinding.inflate(inflater, container, false)
+        binding.gameView.layoutManager = LinearLayoutManager(context)
+
+        itemList = arrayListOf()
+
+        // 리사이클러뷰 어댑터 초기화
+        adapter = FindAdapter(itemList)
+        binding.gameView.adapter = adapter
+
+        // 검색창 이벤트 리스너 등록
+        binding.searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+            // 검색 버튼 입력 시 호출 (검색 버튼이 없으므로 사용하지 않음)
+            override fun onQueryTextSubmit(query: String?): Boolean {
+                return false
+            }
+
+            // 텍스트 입력/수정 시에 호출
+            override fun onQueryTextChange(newText: String?): Boolean {
+                if (newText != null) {
+                    searchGames(newText)
+                }
+                return true
+            }
+        })
 
         return binding.root
     }
 
+    private fun searchGames(searchQuery: String) {
+        // 이전 검색 결과 초기화
+        itemList.clear()
 
+        // Firestore에서 데이터 검색
+        db.collection("game")
+            .whereEqualTo("name", searchQuery)
+            .get()
+            .addOnSuccessListener { documents ->
+                for (document in documents) {
+                    itemList.add(document.id)
+                }
+                adapter.notifyDataSetChanged() // 어댑터 갱신
+            }
+            .addOnFailureListener { exception ->
+                Log.w(TAG, "Error getting documents: ", exception)
+            }
+    }
+
+    companion object {
+        private const val TAG = "FindFragment"
+    }
 }
